c3ed1ab13e024c65c14ebda8970131cc0c667060
lemarek81 <lemarek@poczta.fm>
2021-02-13 20:07:24 +0100

    porduct structuer

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.scss';
+
+import Home from'./components/home/home.js'
+
+
+function App() {
+  return (
+    <div 
+    className='AppContainer'
+    >
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/loginAndRegistration/register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './register.scss';
+
+export default function register() {
+    return (
+        <div>
+            dupa
+        </div>
+    )
+}
